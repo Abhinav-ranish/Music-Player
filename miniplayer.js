de009609cc1a085fa073a52f8278dc4459a78cc3
Abhinav <abhinav.ranish@outlook.com>
2024-11-07 02:50:30 -0700

    Miniplayer

---
+++ miniplayer.js
@@ -0,0 +1,145 @@
+// Update selectors to match miniplayer.html
+let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+
+let seek_slider = document.querySelector(".seek-slider"); // Updated name
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
+let track_index = 0;
+let isPlaying = false;
+let updateTimer;
+
+// Create new audio element
+let curr_track = document.createElement('audio');
+
+// Define the tracks that have to be played
+let track_list = [
+    {
+      name: "Con te Partiro",
+      artist: "Ludig",
+      image: "Music/Covers/ludwig.jpg",
+      path: "Music/Ludwig - Con te partiro.mp3"
+    },
+    {
+      name: "Bella Ciao",
+      artist: "El Profesor",
+      image: "Music/Covers/BellaCiao.jpg",
+      path: "Music/bella ciao.mp3"
+    },
+    {
+      name: "Islands (kompa pasión)",
+      artist: "Tomo, Frozy",
+      image: "Music/Covers/islands.jpg",
+      path: "Music/islands.mp3"
+    },
+    {
+      name: "Snowfall",
+      artist: "øneheart x reidenshi",
+      image: "Music/Covers/snowfall.jpeg",
+      path: "Music/snowfall.mp3"
+    },
+    {
+      name: "Ransom",
+      artist: "Lil Tecca",
+      image: "Music/Covers/ransom.jpeg",
+      path: "Music/Ransom.mp3"
+    },
+    {
+      name: "Savage Love",
+      artist: "Jason Derulo",
+      image: "Music/Covers/savage-love.jpeg",
+      path: "Music/Savage Love.mp3"
+    },
+];  
+
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  resetValues();
+  curr_track.src = track_list[track_index].path;
+  curr_track.load();
+  curr_track.volume = 0.1;  
+
+  track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+  track_name.textContent = track_list[track_index].name;
+  track_artist.textContent = track_list[track_index].artist;
+  now_playing && (now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length);
+
+  updateTimer = setInterval(seekUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+}
+
+function resetValues() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
+}
+
+// Load the first track in the tracklist
+loadTrack(track_index);
+
+function playpauseTrack() {
+  if (!isPlaying) playTrack();
+  else pauseTrack();
+}
+
+function playTrack() {
+  curr_track.play();
+  isPlaying = true;
+  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-2x"></i>';
+}
+
+function pauseTrack() {
+  curr_track.pause();
+  isPlaying = false;
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-2x"></i>';
+}
+
+function nextTrack() {
+  if (track_index < track_list.length - 1)
+    track_index += 1;
+  else track_index = 0;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function prevTrack() {
+  if (track_index > 0)
+    track_index -= 1;
+  else track_index = track_list.length - 1;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function seekTo() {
+  let seekto = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seekto;
+}
+
+function seekUpdate() {
+  let seekPosition = 0;
+
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
